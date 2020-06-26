01737ce2da1a8a3831e195dfad39459d509f98a0
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-06-26 16:23:44 +0200

    Commit vid 12

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 
-// laat style in
+// laat style in + script
 function laadStijlblad()
 {
     wp_enqueue_style('stijl', get_stylesheet_uri());
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), 1.5, true);
 }
 
 add_action('init', "laadStijlblad");
@@ -26,9 +27,9 @@ add_action('init', 'register_my_menus');
 function registreer_menu()
 {
     $argumenten = array(
-        'hoofd-menu' => __('Hooft menu')
+        'hooft-menu' => __('Hooft menu')
     );
-    register_nav_menus();
+    register_nav_menus($argumenten);
 }
 
 add_action('init', 'registreer_menu');
