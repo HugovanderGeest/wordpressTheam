7f216b3d5ce2fccb8f28d77b4bf1658b6f220542
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-06-27 13:21:14 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,16 +13,6 @@ add_action('init', "laadStijlblad");
 
 
 // registreren
-function register_my_menus()
-{
-    register_nav_menus(
-        array(
-            'header-menu' => __('Header Menu'),
-            'extra-menu' => __('Extra Menu')
-        )
-    );
-}
-add_action('init', 'register_my_menus');
 
 function registreer_menu()
 {
